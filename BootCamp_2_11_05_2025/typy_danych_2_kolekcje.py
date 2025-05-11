c73174ab4c7d64d28d3cce40ead00b5fc0bc8b51
Jacek <jaki71@gmail.com>
2025-05-11 12:35:10 +0200

    update day 1 4

--- BootCamp_2_11_05_2025/typy_danych_2_kolekcje.py
+++ BootCamp_2_11_05_2025/typy_danych_2_kolekcje.py
@@ -74,6 +74,168 @@ print(lista[2:3])
 print(lista[4:10])
 print(lista[7:10])
 
+print(10 * "-")
+
+#nadpisanie elementów w liście na wskazany indeks
+
+lista[2] = "Mikołaj"
+print(lista)
+print(len(lista))
+
+print(10 * "-")
+
+# lista.insert(_index: 1, _object: ("Karolina"))
+print(lista)
+print(len(lista))
+
+print(10 * "-")
+
+# usunięcie po indeksie pop
+print(lista.pop(0))
+print(lista)
+print(len(lista))
+ind = lista.index("Zenek")
+print("Numer dla indeksu Zenka", ind)
+print(10 * "-")
+
+
+lista.append("Zennek")
+print(lista)
+print("Numer dla indeksu Zenka", ind)
+print(lista.pop(ind))
+print(lista)
+
+print("Numer dla indeksu Zenka", ind)
+
+# Usunięcie po elemencie
+lista.append("Maciek")
+print(lista)
+lista.remove("Maciek")
+print(lista)
+
+print(10 * "-")
+
+print("Marta" in lista)
+print("Marcin" in lista)
+
+print(10 * "-")
+
+print(lista.remove("Marta"))
+print(lista)
+
+lista.append("Marta")
+lista.append("Marta")
+lista.append("Marcin")
+print(lista)
+print(lista.index("Marta"))
+
+print(10 * "-")
+
+a = 1
+b = 3
+print(f"{a=}, {b=}")
+a = b
+print(f"{a=}, {b=}")
+b = 7
+print(f"{a=}, {b=}")
+
+lista_4 = lista
+print(f"{lista}")
+print(f"{lista_4}")
+lista_copy = lista.copy()
+lista.clear()
+print(f"{lista}")
+print(f"{lista_4}")
+print(f"{lista_copy}")
+
+print(10 * "-")
+# id()
+print(f"adres: {id(lista)=}")
+print(f"adres: {id(lista_4)=}")
+print(f"adres: {id(lista_copy)=}")
+
+print(10 * "-")
+
+liczby = [45, 999, 34, 22, 13.34, 687]
+print(liczby)
+print(type(liczby))
+
+liczby.sort()
+print(liczby)
+print(10 * "-")
+
+liczby_a = [45, 999, 34, 22, 13.34, 687, "A"]
+print(liczby_a)
+print(type(liczby_a))
+
+# liczby_a.sort()  not supported between instances of 'str' and 'int'
+
+lista_osoby = ["Radek", "Tomek", "Zenek", "Ania", "Karolina", "Magda"]
+lista_osoby.sort()
+print(lista_osoby)
+
+lista_alfabet = ["a", "z", "p", "d"]
+lista_alfabet.sort()
+print(lista_alfabet)
+lista_alfabet_pol = ["a", "z", "ą", "p", "ń", "d"]
+lista_alfabet_pol.sort()
+print(lista_alfabet_pol)
+
+print(ord("z"))
+print(ord("ą"))
+
+print(10 * "-")
+
+# sortowanie i odwrocenie w 1 kroku
+liczby.sort(reverse=True)
+print(liczby)
+
+#wypisanie w odwróconej kolejności bez zmiany bazowej listy
+print(liczby[::-1])
+print(liczby[0:4:2])
+print(liczby)
+print(liczby[-3:0:-1])
+
+print(10 * "-")
+
+# odwrócenie kolekcji bez sortowania
+liczby_3 = [3, 8, 5, 12, 1]
+liczby_3.reverse()
+print(liczby_3)
+print(10 * "-")
+
+lista_osoby.reverse()
+print(lista_osoby)
+
+# laczenie list = nowa kolekcja
+
+print(lista + liczby_a)
+liczby_4 = liczby + liczby_3
+print(liczby_4)
+
+print(10 * "-")
+print(liczby)
+
+liczby[3] = 777
+print(liczby)
+liczby[-1] = 666
+print(liczby)
+print(liczby[1:4])
+print(liczby[-4:-2])
+usuniety = liczby.pop(2)
+print(usuniety)
+removniety = liczby.remove(22)
+print(removniety)
+print(liczby)
+print(liczby[::-1])
+
+print(10 * "-")
+
+
+
+
+
+
 
 
 
