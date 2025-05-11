6cbad402f793c22fc0f337aac3ac6732ed5148d9
Jacek <jaki71@gmail.com>
2025-05-11 14:26:22 +0200

    update day 2 1

--- BootCamp_2_11_05_2025/typy_danych_3.py
+++ BootCamp_2_11_05_2025/typy_danych_3.py
@@ -63,3 +63,70 @@ print(sorted(tupla_names)) # tworzy liste z tupli
 print(tupla_names)
 
 print(sorted(tupla_names, reverse=True))
+
+print(10 * "-")
+
+a, b = 1, 2
+print(f"{a=}, {b=}")
+b, a = a, b
+print(f"{a=}, {b=}")
+
+print(type((1, 2)))
+
+tup1 = 1, 2
+print(type(tup1))
+
+a, b = tup1
+print(f"{a=}, {b=}")
+
+tup2 = 1, 2, 3
+print(type(tup2))
+
+# a, b = tup2 - nie zgadza sie rozmiar tupli
+
+a, *b, = tup2
+print(f"{a=}, {b=}")
+
+print(10 * "-")
+
+print(tupla_names)
+
+a, b, c, d, = tupla_names
+print(f"{a=}, {b=}, {c=} {d=}")
+
+a, b, *c = tupla_names
+
+*a, b, c
+print(f"{a=}, {b=}, {c=}")
+
+a, *b, c = tupla_names
+print(f"{a=}, {b=}, {c=}")
+
+print(10 * "-")
+
+tupla_zadanie = "OLA", "Ania", "Ada", "Kasia", "Paulina"
+i1, i2, *i3, i4 = tupla_zadanie
+print(i1, i2, i3, i4)
+
+print("Jeden", "Dwa", "Trzy")
+print("Jeden", "Dwa", "Trzy", sep="")
+print("Jeden", "Dwa", "Trzy", sep="=>")
+print("Jeden", "Dwa", "Trzy", sep=":")
+print("Jeden", "Dwa", "Trzy", sep=":", end="")
+print("Dalszy tekst")
+print("Radek")
+
+print(10 * "-")
+
+lista = list(tupla_names)
+print(type(lista))
+print(len(lista))
+print(lista)
+
+print(10 * "+")
+
+
+
+
+
+
