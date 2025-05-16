b78b5a48837f5bbcc859dd0d3ec697677476cae0
Jacek <jaki71@gmail.com>
2025-05-16 11:22:06 +0200

    trenujemy_d2_2

---
+++ Trening_01/trenujemy_3.py
@@ -0,0 +1,16 @@
+wiek = 54
+rok = 2015
+temp = 36.6
+
+print(temp, type(temp))
+
+print(wiek + rok)
+print(wiek - rok)
+print(wiek * rok)
+print(wiek / rok)
+
+print(rok // wiek)
+print(10 // 3)
+
+print(wiek % rok)
+print(10 % 3)
