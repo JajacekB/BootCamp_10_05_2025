8107aa7ef24356a1f69257c78ea4ca3ecd17194d
Jacek <jaki71@gmail.com>
2025-05-24 10:58:24 +0200

    day_3_1

---
+++ day3_24_05_25/warunki.py
@@ -0,0 +1,44 @@
+# instrukcje warunkowe
+# instrukcjie sterowania przepływem programu
+# instrukcja if sterowana warunkiem
+# if warunek:
+#     komenda(blok) wykonany gdy warunek spełniony
+
+odp = True
+print((bool(odp))) # True
+# odp = False
+if odp:
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza część programu")
+
+odp = "Tomek"
+if odp == "Radek":
+    print("Radek")
+else:
+    print("inny pacjent")
+
+
+# podatek = 0.9
+# zarobki = int(input("Podaj dochód"))
+# if zarobki < 30000:
+#     podatek = 0.0
+# elif zarobki < 60_000:
+#     podatek = 0.2
+# elif zarobki < 120_000:
+#     podatek = 0.4
+# else:   # działanie domyslne
+#     podatek = 0.9
+#
+# print(f"Płacisz {zarobki * podatek} podatek")
+
+
+
+
