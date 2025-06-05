382a388a1e5e6576f0036093da92fabf022ac825
Jacek <jaki71@gmail.com>
2025-06-05 17:06:25 +0200

    trening_2_1

--- day3_24_05_25/petle_zad2.py
+++ day3_24_05_25/petle_zad2.py
@@ -1,21 +1,31 @@
 dictionary = {'imie': "Radek", 'nazwisko': "Kowalski"}
 print(dictionary)
-print(type(di))
+print(type(dictionary))
 
 for i in dictionary:
     print(i)
 
+print("-")
+
 for k in dictionary.keys():
     print(k)
+
+print('+')
 
 for v in dictionary.values():
     print(v)
 
+print('=')
+
 for i in dictionary.items():
     print(i)
 
+print('@')
+
 for k, v in dictionary.items():
     print(k,v, sep="<=>")
+
+print('%')
 
 for k, v in dictionary.items():
     print(k, v, sep=" : ")
