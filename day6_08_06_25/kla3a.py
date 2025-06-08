ad7f29e6b459f89f2c91e10df93a0b61b21acb44
Jacek <jaki71@gmail.com>
2025-06-08 15:33:26 +0200

    boot_6_7

---
+++ day6_08_06_25/kla3a.py
@@ -0,0 +1,75 @@
+from pprint import pprint
+
+
+class ContactList(list['Contact']):
+    """
+    Lista z metodą search
+    """
+
+    def search(self, name):
+        matching_contacts = []
+        for c in self:
+            if name.casefold() in c.name.casefold():
+                matching_contacts.append(c)
+        return matching_contacts
+
+
+
+class Contact:
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    def __repr__(self):
+        return f"{self.name!r} {self.email!r}"
+
+
+class Suplier(Contact):
+    """
+    Klasa Suplier
+    """
+
+    def order(self, order):
+        print(f"{order} zamowiono od {self.name}")
+
+    def __repr__(self):
+        return f"{self.name}, {self.email}"
+
+
+class Friend(Suplier):
+    """
+
+    """
+
+    def __init__(self, name, email, phone="00000000000"):
+        super().__init__(name, email)
+        self.phone = phone
+
+    def __repr__(self):
+        return f"{self.__clas__.name}{self.name}{self.email}+49{self.phone}"
+
+
+
+c1 = Contact("Adam", "adam@wp.pl")
+c2 = Contact("Radek", "radek@wp.pl")
+c3 = Contact("Tomek", "tomek@wp.pl")
+
+print(Contact.all_contacts)
+
+s1 = Suplier("Marek", "marek@wp.pl")
+print(Contact.all_contacts)
+print(s1.all_contacts)
+
+s1.order("kawa")
+
+print(s1.all_contacts.search("Radek"))
+
+f1 = Friend("Marcin", "marcin@o2.pl", 456873259)
+print(f1)
+f1.order("herbata")
+print(f1.all_contacts)
+
+pprint(f1.all_contacts)
---
+++ day6_08_06_25/kla4a.py
@@ -0,0 +1,40 @@
+class DefoultDict(dict):
+    def __missing__(self, key):
+        return "default"
+
+
+d_python = {}
+print(type(d_python))
+print(d_python)
+d_python['name'] = "Radek"
+print(d_python['name'])
+
+d1 = DefoultDict()
+print(d1["name"])
+
+class ZeroDefaultDict(dict):
+    def __missing__(self, key):
+        self[key] = 0
+        return 0
+
+
+d2 = ZeroDefaultDict()
+print(d2)
+print(d2["name"])
+print(d2)
+print(d2['name'])
+d2['name'] = "Radek"
+print(d2)
+
+class CaseInsensitiveDict(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            return self.get(key.casefold())
+        return None
+
+d3 = CaseInsensitiveDict()
+d3['name'] = "Radek"
+print(d3['NAme'])
+d3[1] = "Radek"
+print(d3)
+print(d3)
