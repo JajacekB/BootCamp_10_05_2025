b9ad8fa6ff9f8a5821d1553dfd4a0783b08dff54
Jacek <jaki71@gmail.com>
2025-06-08 14:48:35 +0200

    boot_6_6

---
+++ day6_08_06_25/kla2a.py
@@ -0,0 +1,32 @@
+class Contact:
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    def __repr__(self):
+        return f"{self.name} {self.email}"
+
+
+class Suplier(Contact):
+    """
+    Klasa Suplier
+    """
+
+    def order(self, order):
+        print(f"{order} zamowiono od {self.name}")
+
+c1 = Contact("Adam", "adam@wp.pl")
+c2 = Contact("Radek", "radek@wp.pl")
+c3 = Contact("Tomek", "tomek@wp.pl")
+
+print(Contact.all_contacts)
+
+s1 = Suplier("Marek", "marek@wp.pl")
+print(Contact.all_contacts)
+print(s1.all_contacts)
+
+s1.order("kawa")
+
