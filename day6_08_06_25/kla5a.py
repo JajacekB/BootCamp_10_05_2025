fcaaae5e8e72c28ae51ce3d938177a74980a7381
Jacek <jaki71@gmail.com>
2025-06-08 16:05:52 +0200

    boot_6_8

---
+++ day6_08_06_25/kla5a.py
@@ -0,0 +1,28 @@
+class LongestKeyDict(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if longest is None or len(key) > len(longest):
+                longest = key
+        return longest
+
+
+art = LongestKeyDict()
+art['tomasz'] = 12
+art['abraham'] = 7
+art['zen'] = 17
+print(art.longest_key())
+
+# assert 'abraham' == art.longest_key()
+# assert 'zen' == art.longest_key()
+
+class LongestKeyDictMax(dict):
+    def longest_key(self):
+        return max(self.keys(), key=len)
+
+
+art = LongestKeyDictMax()
+art['abomasz'] = 12
+art['abraham'] = 7
+art['zen'] = 17
+print(art.longest_key())
