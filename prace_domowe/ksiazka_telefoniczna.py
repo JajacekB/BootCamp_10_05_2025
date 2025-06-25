7579762f826f7554cce772799c24091a17a93a82
Jacek <jaki71@gmail.com>
2025-06-25 14:50:09 +0200

    home_work_2_1

--- prace_domowe/ksiazka_telefoniczna.py
+++ prace_domowe/ksiazka_telefoniczna.py
@@ -123,7 +123,7 @@ def find_contact():
     finded = False
     for contact in contacts:
         if contact.get(find_category, "").lower() == find_person.lower():
-            print(contact)
+            print(f"{contact['name']} {contact['last_name']} - {contact['phone']}")
             finded = True
 
 # Wyświetlanie listy kontaktów
