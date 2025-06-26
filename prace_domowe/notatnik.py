bd44692bf6b0efe705816ba2b231f136dcd89395
Jacek <jaki71@gmail.com>
2025-06-26 07:05:43 +0200

    home_work_2_2

---
+++ prace_domowe/notatnik.py
@@ -0,0 +1,14 @@
+# Prosty notatnik z menu
+#
+# Stwórz aplikację konsolową, która umożliwia:
+# 	•	dodawanie notatek (krótki tekst),
+# 	•	usuwanie notatek (po numerze/id),
+# 	•	edycję notatki,
+# 	•	przeglądanie wszystkich notatek.
+#
+# Użyj pętli while True, prostej listy (lub słownika) oraz funkcji.
+
+import os
+import csv
+
+
