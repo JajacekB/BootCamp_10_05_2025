f50dd97a6a090e0363144ef203c98f2f6879a480
Jacek <jaki71@gmail.com>
2025-06-28 09:45:36 +0200

    api_9_1

---
+++ day9_28_06_25/api_numbers.py
@@ -0,0 +1,20 @@
+import requests
+
+url="http://numbersapi.com/random/year?json"
+
+response = requests.get(url)
+print(response)
+data = response.json()
+print(type(data))
+
+print(data)
+
+print("Co wieesz o roku:\n",data['text'].replace(str(data['number']), ""))
+print(data["text"],)
+
+user_answer = input("\n???: ")
+
+if user_answer == str(data["number"]):
+    print("Brawo!!!")
+else:
+    print("Słabo, zła odpowiedź!!!")
