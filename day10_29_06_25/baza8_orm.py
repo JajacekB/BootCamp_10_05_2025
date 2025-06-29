59e60fb1302c44df83a7486bba363e31ac37bfd8
Jacek <jaki71@gmail.com>
2025-06-29 15:12:20 +0200

    baza_10_3

---
+++ day10_29_06_25/baza8_orm.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker, declarative_base, aliased
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+    def __repr__(self):
+        return  f"<User(name={self.name}, ege={self.age}"
+
+
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
+Base.metadata.create_all(engine)
+
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+new_user = User(name="Wojtek Kulka", age=37)
+session.add(new_user)
+
+session.commit()
+session.close()
+
+users = session.query(User).all()
+for user in users:
+    print(user)
+    print(f"Imię: {user.name} wiek: {user.age}")
+
+result = session.execute(text("SELECT * FROM users"))
+for row in result:
+    print(row)
+
+
+stmt = text("SELECT * FROM users")
+result = session.query(User).from_statement(stmt).all()
+
+for user in users:
+    print(type(user))
+    print(user.name)
