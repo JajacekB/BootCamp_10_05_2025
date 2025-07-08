09cc85bd9f44add6ef25773add64cfff8bfd7619
Jacek <jaki71@gmail.com>
2025-07-08 21:46:14 +0200

    homework_7_3

--- prace_domowe/fleet_manager_3000/vehicle.py
+++ prace_domowe/fleet_manager_3000/vehicle.py
@@ -16,7 +16,7 @@ class Vehicle(ABC):
         :param return_date:
         """
 
-        self.__id = vehicle_id
+        self.__vehicle_id = vehicle_id
         self.__brand = brand
         self.__cash_per_day = cash_per_day
         self.__is_available = is_available
@@ -37,7 +37,7 @@ class Vehicle(ABC):
         return True
 
 
-    def return(self):
+    def return_vehicle(self):
         if self.__is_available:
             return True
 
@@ -47,7 +47,7 @@ class Vehicle(ABC):
 
     def __str__(self):
         return_date_str = self.__return_date.strftime("%Y-%m-%d") if self.__return_date else "brak"
-        return f"""ID: {self.__id}
+        return f"""ID: {self.__vehicle_id}
     Marka: {self.__brand}
     Cena/dzień: {self.__cash_per_day} zł
     Dostępny: {self.__is_available}
@@ -55,47 +55,10 @@ class Vehicle(ABC):
     Data zwrotu: {return_date_str}
     """
 
-
-
-
-class Car(Vehicle):
-    def __init__(self, vehicle_id, brand, cash_per_day, is_available, sits, engin):
-        super().__init__(vehicle_id, brand, cash_per_day, is_available)
-        self.sits = sits
-        self.engin = engin
-
-
-class Scooter(Vehicle):
-    def __init__(self, max_speed):
-        """
-
-        :param max_speed:
-        """
-        self.max_speed = max_speed
-
-
-class Bike(Vehicle):
-    def __init__(self, category):
-        """
-
-        :param category:
-        """
-        self.type = category
-
-
-
-
-
-
-
-
-
-
-
     # konwersja obiektu klasy Vehicle do słownika dla późniejszego zapisu w pliku .json
     def to_dict(self):
         return {
-            "id": self.__id,
+            "id": self.__vehicle_id,
             "brand": self.__brand,
             "cash_per_day": self.__cash_per_day,
             "is_available": self.__is_available,
@@ -112,6 +75,112 @@ class Bike(Vehicle):
             brand=data["brand"],
             cash_per_day=data["cash_per_day"],
             is_available=data["is_available"],
+            borrower=data["borrower"],
+            return_date=datetime.strptime(data["return_date"], "%Y-%m-%d").date() if data["return_date"] else None
+        )
+
+
+
+
+class Car(Vehicle):
+    def __init__(self, vehicle_id, brand, cash_per_day, is_available, seats, fuel_type, borrower=None, return_date=None):
+        super().__init__(vehicle_id, brand, cash_per_day, is_available, borrower, return_date)
+        self.seats = seats
+        self.fuel_type = fuel_type
+
+    def get_type(self):
+        return "Samochód"
+
+    def __str__(self):
+        return super().__str__() + f"""Liczba miejsc: {self.seats}
+    Typ paliwa: {self.fuel_type}
+    """
+
+    # Konwersja obiektu klasy Car do słownika dla późniejszego zapisu w pliku .json z nadpisaniem klasy Vehicle
+    def to_dict(self):
+        data = super().to_dict()  # pobiera dane z Vehicle
+        data.update({
+            "seats": self.seats,
+            "fuel_type": self.fuel_type
+        })
+        return data
+
+    # Konwersja z .json do obiektu klasy Car
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            vehicle_id=data["id"],
+            brand=data["brand"],
+            cash_per_day=data["cash_per_day"],
+            is_available=data["is_available"],
+            seats=data["seats"],
+            fuel_type=data["fuel_type"],
+            borrower=data["borrower"],
+            return_date=datetime.strptime(data["return_date"], "%Y-%m-%d").date() if data["return_date"] else None
+        )
+
+
+class Scooter(Vehicle):
+    def __init__(self, vehicle_id, brand, cash_per_day, is_available, max_speed, borrower=None, return_date=None):
+        super().__init__(vehicle_id, brand, cash_per_day, is_available, borrower, return_date)
+        self.max_speed = max_speed
+
+    def get_type(self):
+        return "Scooter"
+
+    def __str__(self):
+        return super().__str__() + f"Maksymalna prędkość: {self.max_speed}km/h\n"
+
+    # Konwersja obiektu klasy Scooter do słownika dla późniejszego zapisu w pliku .json z nadpisaniem klasy Vehicle
+    def to_dict(self):
+        data = super().to_dict()  # pobiera dane z Vehicle
+        data.update({"max_speed": self.max_speed})
+        return data
+
+    # Konwersja z .json do obiektu klasy Scooter
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            vehicle_id=data["id"],
+            brand=data["brand"],
+            cash_per_day=data["cash_per_day"],
+            is_available=data["is_available"],
+            max_speed=data["max_speed"],
+            borrower=data["borrower"],
+            return_date=datetime.strptime(data["return_date"], "%Y-%m-%d").date() if data["return_date"] else None
+        )
+
+
+class Bike(Vehicle):
+    def __init__(self,vehicle_id, brand, cash_per_day, is_available, bike_type, is_electric, borrower=None, return_date=None):
+        super().__init__(vehicle_id, brand, cash_per_day, is_available, borrower, return_date)
+        self.bike_type = bike_type
+        self.is_electric = is_electric
+
+    def get_type(self):
+        return "Rower"
+
+    def __str__(self):
+        return super().__str__() + (f"""Typ roweru: {self.bike_type}
+        Elektryczny: {'Tak' if self.is_electric else 'Nie'}\n""")
+
+    def to_dict(self):
+        data = super().to_dict()  # pobiera dane z Vehicle
+        data.update({
+            "bike_type": self.bike_type,
+            "is_electric": self.is_electric
+        })
+        return data
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            vehicle_id=data["id"],
+            brand=data["brand"],
+            cash_per_day=data["cash_per_day"],
+            is_available=data["is_available"],
+            bike_type=data["bike_type"],
+            is_electric=data["is_electric"],
             borrower=data["borrower"],
             return_date=datetime.strptime(data["return_date"], "%Y-%m-%d").date() if data["return_date"] else None
         )
