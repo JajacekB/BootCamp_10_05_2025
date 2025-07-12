e7ee5fad1ba3538d9deba9275c54c6b84492d35b
Jacek <jaki71@gmail.com>
2025-07-12 09:55:23 +0200

    day11_12_07_25

--- day10_29_06_25/baza9_postgres.py
+++ day10_29_06_25/baza9_postgres.py
@@ -1,4 +1,5 @@
 import psycopg2
+
 # pip install psycopg2
 
 # docker run --name my-postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
@@ -17,3 +18,14 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
 
 conn.commit()
+
+cursor.execute("INSERT INTO users (name) VALUES (%s)", ("Jan",))
+cursor.execute("INSERT INTO users (name) VALUES (%s)", ("Anna",))
+conn.commit()
+
+cursor.execute("SELECT * FROM users;")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
+conn.close()
