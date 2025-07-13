774cb676c55b34c0a06331d177aad7b7e9f8ff2c
Jacek <jaki71@gmail.com>
2025-07-13 10:14:53 +0200

    day12_13_07_25_u0

--- day12_13_07-25/baza_krok2_tranzakcja.py
+++ day12_13_07-25/baza_krok2_tranzakcja.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from baza_krok1 import Users, Post
 
 
 DATABASE_URI = 'mysql+pymysql://root:abc123@localhost:3306/zaawansowana_baza'
@@ -7,3 +8,21 @@ DATABASE_URI = 'mysql+pymysql://root:abc123@localhost:3306/zaawansowana_baza'
 engine = create_engine(DATABASE_URI, echo=True)
 Base = declarative_base()
 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+try:
+
+    new_user = Users(name="Jan Kowalski", email='jan.kowalski@wp.pl')
+    session.add(new_user)
+
+    new_user = Post(title="Pierwszy post", content='To jest treść pierwszego posta.', user=new_user)
+    session.add(new_user)
+
+    session.commit()
+except Exception as e:
+    print("Błąd:", e)
+    session.rollback()
+
+finally:
+    session.close()
