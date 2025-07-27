14df774181e11f09e19ddc2f51b7ff26db18e0ca
Jacek <jaki71@gmail.com>
2025-07-27 13:32:22 +0200

    day14_27_07_25_u07

---
+++ day_14_27_07_25/testowanie/tests/pytest_zad2.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def calculate_square(num):
+    return num ** 2
+
+def test_squer_1():
+    assert calculate_square(2) == 4
+
+@ pytest.mark.parametrize("input, expected_output", [(2, 4), (3, 9), (4, 16)])
+def test_square_calculate_parametrized(input, expected_output):
+
+    assert calculate_square(input) == expected_output
