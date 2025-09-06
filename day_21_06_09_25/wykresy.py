8198f827ec97facbbb2513c981415a6742eab879
Jacek <jaki71@gmail.com>
2025-09-06 11:58:25 +0200

    day_21_u09

--- day_21_06_09_25/wykresy.py
+++ day_21_06_09_25/wykresy.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-
+# pip install matplotlib
 import numpy as np
-from matplotlib.patches import bbox_artist
 
-y = np.linspace(0, 10, 100)
+y = np.linspace(0, 10, 100)  # dane z zakreso od 0 do 10 podzielone na 100 części
 x = np.linspace(0, 10, 100)
 
 plt.plot(x, y)
@@ -13,16 +12,11 @@ plt.ylabel("Oś Y")
 
 plt.title("Wykres liniowy")
 # plt.show()
+# plt.savefig('wykres.png')
+# plt.close()
 
-plt.savefig('wykres.png')
+# plt.savefig("wykres2.png", dpi=300)
+# plt.close()
+
+plt.savefig("wykres3", dpi=300, bbox_inches='tight')
 plt.close()
-
-plt.savefig('wykres2.png', dpi = 300)
-plt.close()
-
-plt.savefig('wykres3.png', dpi = 300, bbox_inches.size =3)
-plt.close()
-
-
-
-
