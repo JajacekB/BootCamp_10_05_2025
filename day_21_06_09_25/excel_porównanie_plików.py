a7ebb464d8e5e1fb0d384579a81bc14d8640aaa3
Jacek <jaki71@gmail.com>
2025-09-06 10:04:29 +0200

    day_21_u02

---
+++ day_21_06_09_25/excel_porównanie_plików.py
@@ -0,0 +1,15 @@
+from openpyxl import load_workbook
+
+wb1 = load_workbook("tabela_przestawna.xlsx")
+wb2 = load_workbook("vgsales_formated.xlsx")
+
+ws1 = wb1.active
+ws2 = wb2.active
+
+print(ws1)
+print(ws2)
+
+if ws1['A1'].value == ws2['A1'].value:
+    print("Wartości A! są równe")
+else:
+    print(f"Różnica: plik1 = {ws1['A1'].value}, plik2 = {ws2['A1'].value}")
