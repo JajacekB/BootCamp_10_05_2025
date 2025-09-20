9917e2fbc8d4be4baf9f6444534a185a889afe39
Jacek <jaki71@gmail.com>
2025-09-20 11:35:55 +0200

    day_23_update_06

--- day_23_20_09-25/pandas_ex6.py
+++ day_23_20_09-25/pandas_ex6.py
@@ -7,3 +7,52 @@ df.fillna(130, inplace=True)
 print(df.info())
 
 print(df.loc[141])
+
+df = pd.read_csv("data.csv")
+df.fillna({"Calories":130}, inplace=True)
+print(df.info())
+
+print(df.loc[141])
+
+df = pd.read_csv('data.csv')
+df["Calories"] = df['Calories'].fillna(130)
+df.info()
+print(df.loc[141])
+
+# df['Calories'].fillna(130, inplace=True)
+df.info()
+print(df.loc[141])
+
+df = pd.read_csv('data.csv')
+
+x = df['Calories'].mean()
+print("Średnia wynosi: ", x)
+
+df['Calories'] = df["Calories"].fillna(x)
+print(df.loc[141])
+x = df['Calories'].mean()
+print("Średnia wynosi: ", x)
+
+data = {"Wiek": [25, 30, 35, 40, 45, 50, 55, 60, 65]}
+
+df = pd.DataFrame(data)
+mediana_wiek = df["Wiek"].median()
+print("Mediana wieku: ", mediana_wiek)
+
+df = pd.read_csv('data.csv')
+mediana_cal = df["Calories"].median()
+df['Calories'] = df["Calories"].fillna(mediana_cal)
+print("Mediana cal: ", mediana_cal)
+print(df.loc[141])
+
+
+
+
+
+
+
+
+
+
+
+
