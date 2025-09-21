837b8612fde26745da8f83c5fd37a8e3cca8a1d8
Jacek <jaki71@gmail.com>
2025-09-21 10:21:00 +0200

    day_24_update_01

--- day_23_20_09-25/marketing_zadanie/marketing_zad1.py
+++ day_23_20_09-25/marketing_zadanie/marketing_zad1.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
+from sympy import rotations
+
 
 df = pd.read_csv('marketing_r.csv', sep=",")
 
-print(df)
-
 print(df.describe())
+
 
 df.info()
 
-print(df['converted'].dtype) # object
+print(df['converted'].dtype)  # object
 
-df['converted'] = df['converted'].astype(bool)
-print(df['converted'].dtype)
+# zmienić typ kolumny na typ bool
+df['converted'] = df['converted'].astype('bool')
+print(df['converted'].dtype)  # bool
 df.info()
 
 print(df.head(1).to_string())
@@ -21,3 +23,51 @@ df['is_house_ads'] = np.where(df['marketing_channel'] == "House Ads", True, Fals
 print(df.is_house_ads.head(3))
 
 df.to_csv('marketing_is_house_ads.csv', sep=",", index=False)
+
+# zamiana dat na typ datetime
+df['date_served'] = pd.to_datetime(df['date_served'], errors='coerce', format='mixed')
+print(df['date_served'].head(3))
+# 0   2018-01-01
+# 1   2018-01-01
+# 2   2018-01-01
+# Name: date_served, dtype: datetime64[ns]
+
+# # dni tygodnia numerycznie
+# df['date_served'] = df['date_served'].dt.dayofweek
+# print(df['date_served'].head(3))
+# # 0    0.0 - poniedziałek
+# # 1    0.0
+# # 2    0.0
+# # Name: date_served, dtype: float64
+
+# nazwy dni tygodnia
+# df['day_name'] = df['date_served'].dt.day_name()
+# print(df['day_name'].head(3))
+# 0    Monday
+# 1    Monday
+# 2    Monday
+# Name: day_name, dtype: object
+# dodanie kolumny channel_code, zmapowanie nazw  marketing_channel na channel_code
+channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push": 5}
+df['channel_code'] = df['marketing_channel'].map(channel_dict)
+print(df['channel_code'].head(3))
+# 0    1.0
+# 1    1.0
+# 2    1.0
+# Name: channel_code, dtype: float64
+
+# unikalni uzytkownicy dziennie
+daily_users = df.groupby(['date_served'])['user_id'].nunique()
+print("Dziennie:", daily_users)
+
+
+import matplotlib.pyplot as plt
+
+daily_users.plot()
+
+plt.title("Zasięg dzienny kampani marketingowej")
+plt.xlabel("Data")
+plt.ylabel("Liczba użytkowników")
+plt.xticks(rotation=45)
+
+plt.show()
---
+++ day_23_20_09-25/marketing_zadanie/marketing_zad2.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv('marketing_r.csv', sep=",")
+# print(df.head(1).to_string())
+
+# sprawdzmy jaki typ m kolumna 'converted'
+print(df['converted'].dtype)  # object
+
+df['is_house_ads'] = np.where(df['marketing_channel'] == "House Ads", True, False)
+print(df.is_house_ads.head(3))
+
+# zmienić typ kolumny na typ bool
+df['converted'] = df['converted'].astype('bool')
+print(df['converted'].dtype)  # bool
+df.info()
+
+df['date_served'] = pd.to_datetime(df['date_served'], errors='coerce', format='mixed')
+print(df['date_served'].head(3))
+
+channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push": 5}
+df['channel_code'] = df['marketing_channel'].map(channel_dict)
+print(df['channel_code'].head(3))
+
+daily_users = df.groupby(['date_served'])['user_id'].nunique()
+print("Dziennie:", daily_users)
+
+df.to_csv("marketing_ok_date.csv")
+
+subscribers = df[df['converted'] == True]['user_id'].nunique()
+total = df['user_id'].nunique()
+print("Subscribers:", subscribers)
+print("Total", total)
+
+conv_rate = subscribers / total
+print("Convert rate", conv_rate)
+print('Convert rate:', round(conv_rate * 100, 2), "%")
+
+retained = df[df["is_retained"] == True]['user_id'].nunique()
+retained = retained / subscribers
+print("Retention:", round(retained * 100, 2), "%")
+
+house_ads = df[df["subscribing_channel"] == "House Ads"]
+retained = house_ads[house_ads["is_retained"] == True]["user_id"].nunique()
+subscribers = house_ads[house_ads["converted"] == True]["user_id"].nunique()
+retension_rate = retained / subscribers
+print("Retention Rate:", round(retension_rate * 100, 2), "5")
+
+retained = df[df["is_retained"] == True].groupby(['subscribing_channel'])["user_id"].nunique()
+print(retained)
+
+subscribers = df[df['converted'] == True].groupby(['subscribing_channel'])['user_id'].nunique()
+print(subscribers)
+
+channel_retention_rate = (retained / subscribers) * 100
+print(channel_retention_rate)
+
+
