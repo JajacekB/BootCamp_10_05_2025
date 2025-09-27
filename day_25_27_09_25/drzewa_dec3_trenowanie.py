87d0cab6208308673ad7996766266e16d02eba1f
Jacek <jaki71@gmail.com>
2025-09-27 11:34:37 +0200

    day_25_update_03

--- day_25_27_09_25/drzewa_dec3_trenowanie.py
+++ day_25_27_09_25/drzewa_dec3_trenowanie.py
@@ -12,3 +12,17 @@ X = np.array([
 
 y = np.array([0, 1, 1, 1, 0])
 
+# trenowanie drzewa
+tree = DecisionTreeClassifier(criterion='gini', max_depth=2)
+tree.fit(X, y)
+
+decision = tree.predict(np.array([[30, 40]]))
+print("Pozyczka przyznana" if decision[0] == 1 else "Pozyczka odrzucona")
+
+plt.figure(figsize=(8,5))
+plot_tree(tree, feature_names=["wiek", "zarobki"],
+          class_names=['odrzucona', 'przyznana'], filled=True
+          )
+plt.title("Drzewo decyzyjne pozyczki")
+plt.tight_layout()
+plt.show()
